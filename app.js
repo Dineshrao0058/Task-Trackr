a006a7c1cf763a758d1dc425be4e4d3b6d4f54a0
shivanitalla <tallashivani.vita@gmail.com>
2024-08-08 11:23:10 +0530

    student  CRUD completed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Task Trackr!");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./student/routes/studentRoute")
+const app = express();
+app.use(express.json());
+port = 4000;
+app.listen(port, () => {
+    console.log("port succes", port)
+});
+const url = "mongodb://localhost:27017/Tracker-Backend";
+mongoose.connect(url)
+.then(() => console.log("DB is Connected"))
+.catch((error)=>{console.log(error)});
+app.use("/student",router);
---
+++ student/model/studentModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const studentDetail = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        },
+        course:{
+            type:String,
+            require:true
+        },
+        photo:{
+            type:String,
+        } 
+    },
+    {
+        timestamps:true
+    }
+);
+module.exports = mongoose.model("student",studentDetail)
